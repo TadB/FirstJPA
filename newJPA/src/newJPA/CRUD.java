4aa62d5b32778a14f28098f7142a5b7f5c4efdba
TadB <tadeusz.brzytwa@gmail.com>
2018-05-20 12:18:00 +0200

    dodano obiekt testowy

--- newJPA/src/newJPA/CRUD.java
+++ newJPA/src/newJPA/CRUD.java
@@ -1,13 +1,25 @@
 package newJPA;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class CRUD {
 
 	public static void main(String[] args) {
 		EmployeeCRUD employeeCRUD = new EmployeeCRUD();
-
+//-----------DANE TESTOWE --------------		
 		Employee employee = new Employee();
 		Employee data = new Employee();
 		int id = 200;
+		employee.setFirstName("Adam");
+		employee.setLastName("Nowak");
+		employee.setEmployeeId(new BigDecimal("333"));
+		Date hireDate = new Date();
+		employee.setHireDate(hireDate);
+		employee.setManagerId(new BigDecimal("100"));
+		employee.setDepartmentId(new BigDecimal("50"));
+		employee.setSalary(new BigDecimal("6040"));
+//-------------------------------------------
 		
 		employeeCRUD.createEmployee(employee);
 		data = employeeCRUD.readEmployee(id);
